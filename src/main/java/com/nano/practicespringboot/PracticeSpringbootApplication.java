d74d9b3034bcaef84a7043eb7aa266d22b65682d
nanobotrs <raansopro324@gmail.com>
2024-02-08 08:33:34 -0500

    Creacion del proyecto con springboot initializer

---
+++ src/main/java/com/nano/practicespringboot/PracticeSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.nano.practicespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nano/practicespringboot/PracticeSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nano.practicespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
