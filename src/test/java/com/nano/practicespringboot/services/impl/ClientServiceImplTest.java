eb0629c8544316eb9f01a0ddcd4e3545e713c6d8
nanobotrs <raansopro324@gmail.com>
2024-02-15 09:01:38 -0500

    Implementacion de pruebas unitarias con mockito

---
+++ src/test/java/com/nano/practicespringboot/services/impl/ClientServiceImplTest.java
@@ -0,0 +1,16 @@
+package com.nano.practicespringboot.services.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClientServiceImplTest {
+
+    @Test
+    void saveClient() {
+    }
+
+    @Test
+    void updateClient() {
+    }
+}
